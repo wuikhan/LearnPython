808abb8c0aa622d2747b325228cd403452ec8fa6
Waqas Khan <waqas.khan@perficient.com>
2017-09-26 00:39:00 -0400

    ReadingAndWritingFiles.py and .png added

--- ReadingAndWritingFiles.py
+++ ReadingAndWritingFiles.py
@@ -1,9 +1,14 @@
+#Read and write files using the built-in Python files methods
+
 def main():
+    #open a file for writing and create it if it does not exist
     f = open("txtfile.txt","w+")
     
+    #write some lines of data to the file
     for i in range(10):
         f.write("This is line %d\r\n" % (i+1))
         
+    #close the file when done    
     f.close()
 
 if __name__ == "__main__":
